cab8a59f16c2168033fdb5762e2a76f18fb4f424
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-16 14:38:31 +0100

    Switch to bare-bones implementation of React Native.

--- front/App.tsx
+++ front/App.tsx
@@ -1,35 +1,20 @@
-import { useState, useEffect } from 'react';
-import { Provider } from 'react-redux';
-import * as SplashScreen from 'expo-splash-screen';
-import Navigator from '@navigator';
-import store from '@utils/store';
-import { loadImages, loadFonts } from '@theme';
-import 'react-native-gesture-handler';
-
-// keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [isReady, setReady] = useState<boolean>(false);
-
-  // assets preloading
-  const preloadAssets = async () => {
-    try {
-      await Promise.all([loadImages(), loadFonts()]);
-    } finally {
-      setReady(true);
-      SplashScreen.hideAsync();
-    }
-  };
-
-  useEffect(() => {
-    preloadAssets();
-  }, []);
-
-  if (!isReady) return null;
   return (
-    <Provider store={store}>
-      <Navigator />
-    </Provider>
+    <View style={styles.container}>
+      <Text>Try again this live? up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
